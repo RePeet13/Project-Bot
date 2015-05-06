8ed60adb5dbba18596582f7596ed32bfa81c5ce2
Taylor Peet <repeet13@gmail.com>
2015-05-05 23:13:03 -0700

    added gitignore, can now successfully add project directories

--- bin/project-bot.py
+++ bin/project-bot.py
@@ -7,21 +7,28 @@ zeros=4
 
 def main(argv):
     in_project_name = ''
+    in_dir_name = ''
+
+    """ Help Text """
+    help = 'project-bot.py -p "Project Name"'
+
     try:
-        opts, args = getopt.getopt(argv, "hp:", ["project="])
+        opts, args = getopt.getopt(argv, "hp:d:", ["project=","directory="])
     except getopt.GetoptError:
-        print 'project-bot.py -p "Project Name"'
+        print help
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print 'project-bot.py -p "Project Name"'
+            print help
             sys.exit()
         elif opt in ("-p", "--project"):
             in_project_name = arg
             print(in_project_name)
-    if in_project_name == '':
-        project_name = input("Project name, sir: ")
-    else:
+        elif opt in ("-d", "--directory"):
+            in_dir_name = arg
+            print(in_dir_name)
+    if in_project_name != '':
+        global project_name 
         project_name = in_project_name
 
 if __name__ == "__main__":
@@ -29,11 +36,17 @@ if __name__ == "__main__":
     
    
 def create_project():
+
+    global project_name
+    if project_name == '':
+        project_name = input("Project name, sir: ")
+
+    project_dir = getDefaultProjectDir()
+    print("Project Dir: " + str(os.listdir(project_dir)))
     
     existing_dirs = [x for x in os.listdir(project_dir) if not os.path.isfile(os.path.join(project_dir,x)) and x[0] != '.' and x != "bin"]
-    # print(existing_dirs)
     existing_dirs = sorted(existing_dirs)
-    # print(existing_dirs)
+    print("Narrowed Dirs: "+ str(existing_dirs))
     
     if len(existing_dirs) == 0:
         num = 0
@@ -44,8 +57,22 @@ def create_project():
     # print(last_proj)
     
     new_path = project_dir + str(num).zfill(zeros) + "-" + project_name
+    print("Making dir: " + new_path)
     os.mkdir(new_path)
     
+def getScriptPath():
+    return os.path.dirname(os.path.realpath(sys.argv[0]))
+
+def getDefaultProjectDir():
+    dirs_tmp = getScriptPath().split("/")
+    dirs = []
+    for d in dirs_tmp:
+        if d == "bin":
+            break
+        else:
+            dirs.append(d)
+    print("Default Project Dir: " + "/".join(dirs)+"/")
+    return "/".join(dirs) + "/"
 
 """ Actual Execution """
 create_project()
