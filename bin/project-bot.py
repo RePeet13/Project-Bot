79ad490d05d0e507817a33cfce6a5901bbbaea38
Taylor Peet <repeet13@gmail.com>
2015-06-09 01:17:25 +0000

    Converted crappy print statements to Shiny Logging

--- bin/project-bot.py
+++ bin/project-bot.py
@@ -1,4 +1,4 @@
-import os, sys, argparse, json, sys, re, shutil, datetime
+import os, sys, argparse, json, sys, re, shutil, datetime, logging
 from collections import namedtuple
 
 ### Global Project defaults ###
@@ -31,7 +31,7 @@ def genExampleFolder():
     if (len(existing_dirs) > 0):
         for d in existing_dirs:
             if (d.lower().find("example") >= 0):
-                print('Removing old example folder: ' + d)
+                logging.info('Removing old example folder: ' + d)
                 shutil.rmtree(d)
     
     # Create Example just like a normal project
@@ -48,7 +48,7 @@ def create_project(o):
     if options['name'] == '':
         options['name'] = raw_input("Project name, sir: ")
         
-    print('Creating project: ' + options['name'])
+    logging.info('Creating project: ' + options['name'])
 
     existing_dirs = getProjectDirs(options['directory'])
 
@@ -57,11 +57,10 @@ def create_project(o):
         last_proj = existing_dirs[len(existing_dirs)-1]
         if (last_proj.find('-') > -1):
             num = int(last_proj.split("-")[0]) + 1
-            
-    # print(last_proj)
+        logging.debug(last_proj)
     
     options['path'] = options['directory'] + str(num).zfill(zeros) + "-" + options['name']
-    print("Making dir: " + options['path'])
+    logging.info("Making dir: " + options['path'])
     
     os.mkdir(options['path'])
     
@@ -69,10 +68,10 @@ def create_project(o):
     
     
 def getProjectDirs(d):
-    # print("Project Dir: " + str(os.listdir(d)))
+    logging.debug("Project Dir: " + str(os.listdir(d)))
     existing_dirs = [x for x in os.listdir(d) if not os.path.isfile(os.path.join(d, x)) and x[0] != '.' and x != 'bin']
     existing_dirs = sorted(existing_dirs)
-    # print("Narrowed Dirs: " + str(existing_dirs))
+    logging.debug("Narrowed Dirs: " + str(existing_dirs))
     return existing_dirs
     
     
@@ -89,12 +88,12 @@ def getDefaultProjectDir():
             break
         else:
             dirs.append(d)
-    print("Default Project Dir: " + "/".join(dirs)+"/")
+    logging.debug("Default Project Dir: " + "/".join(dirs)+"/")
     return "/".join(dirs) + "/"
     
     
 def parseTemplate(options):
-    print('Parsing template: ' + options['template_name'] + ' for project: ' + options['name'])
+    logging.info('Parsing template: ' + options['template_name'] + ' for project: ' + options['name'])
     global gen
     global val
     global proj_name
@@ -102,80 +101,80 @@ def parseTemplate(options):
     proj_name = options['name']
     
     try:
-        print('Attempting to load: ' + os.path.join(options['template_path'], options['template_name'], 'generic.json'))
+        logging.info('Attempting to load: ' + os.path.join(options['template_path'], options['template_name'], 'generic.json'))
         gen_file = open(os.path.join(options['template_path'], options['template_name'], 'generic.json'), 'r')
         gen = json.load(gen_file)
         gen_file.close()
     except IOError as e:
-        print "I/O error({0}) loading generic.json: {1}".format(e.errno, e.strerror)
+        logging.error("I/O error({0}) loading generic.json: {1}".format(e.errno, e.strerror))
         sys.exit()
     except:
-        print "Unexpected error loading generic.json:", sys.exc_info()[0]
+        logging.error("Unexpected error loading generic.json: " + sys.exc_info()[0])
         raise
         sys.exit()
-    print('..loaded')
+    logging.info('..loaded')
     
     # generic.json File loaded and parsed correctly
     
     try:
-        print('Attempting to load: ' + os.path.join(options['template_path'], options['template_name'], 'values.json'))
+        logging.info('Attempting to load: ' + os.path.join(options['template_path'], options['template_name'], 'values.json'))
         val_file = open(os.path.join(options['template_path'], options['template_name'], 'values.json'), 'r')
         val = json.load(val_file)
         val_file.close()
     except IOError as e:
-        print "I/O error({0}) loading values.json: {1}".format(e.errno, e.strerror)
+        logging.error("I/O error({0}) loading values.json: {1}".format(e.errno, e.strerror))
         sys.exit()
     except:
-        print "Unexpected error loading values.json:", sys.exc_info()[0]
+        logging.error("Unexpected error loading values.json: " + sys.exc_info()[0])
         raise
         sys.exit()
-    print('..loaded')
+    logging.info('..loaded')
 
     # values.json File loaded and parsed correctly
     
-    print('Creating Structure')
+    logging.info('Creating Structure')
     for s in gen['structure']:
         if s['type'] == "folder":
-            print('Type: Folder \n  Location: ' + os.path.join(options['path'], s['name']))
+            logging.debug('Type: Folder \n  Location: ' + os.path.join(options['path'], s['name']))
             os.mkdir(os.path.join(options['path'], s['name']))
         elif s['type'] == "file":
-            print('Type: File')
+            logging.debug('Type: File')
             if s['name'] == 'readme.md':
                 generateReadme(options, s)
     
 
 def generateReadme(options, structure):
+    logging.info('Loading readme files')
     # Try to load src file
     try:
-        print('Attempting to load: ' + os.path.join(options['template_path'], options['template_name'], structure['template']))
+        logging.info('Attempting to load: ' + os.path.join(options['template_path'], options['template_name'], structure['template']))
         src_file = open(os.path.join(options['template_path'], options['template_name'], structure['template']), 'r')
     except IOError as e:
-        print "I/O error({0}) loading readme template: {1}".format(e.errno, e.strerror)
+        logging.error("I/O error({0}) loading readme template: {1}".format(e.errno, e.strerror))
         sys.exit()
     except:
-        print "Unexpected error loading readme template:", sys.exc_info()[0]
+        logging.error("Unexpected error loading readme template: " + sys.exc_info()[0])
         sys.exit()
         raise
-    print('..loaded')
+    logging.info('..loaded')
     # Src file loaded properly (at least we hope)
     
     # Open destination file for writing!
     try:
-        print('Attempting to load: ' + os.path.join(options['path'], structure['path'], structure['name']))
+        logging.info('Attempting to load: ' + os.path.join(options['path'], structure['path'], structure['name']))
         temp_file = open(os.path.join(options['path'], structure['name']), 'w')
     except IOError as e:
-        print "I/O error({0}) creating readme: {1}".format(e.errno, e.strerror)
+        logging.error("I/O error({0}) creating readme: {1}".format(e.errno, e.strerror))
         sys.exit()
     except:
-        print "Unexpected error creating readme:", sys.exc_info()[0]
+        logging.error("Unexpected error creating readme: " + sys.exc_info()[0])
         sys.exit()
         raise
-    print('..loaded')
+    logging.info('..loaded')
     # Opened successfully
     
+    logging.info('Replacing variables with values')
     # Now just to go through line by line and substitute variables in src
-    
-    # TODO likely not matching because of the dash in vAuthor-Name
     pat = re.compile(r'<(.+)>',re.I)
     
     for line in src_file:
@@ -183,6 +182,7 @@ def generateReadme(options, structure):
         l = re.sub(pat,readmeSub,line)
       
         temp_file.write(l)
+    logging.info('..done')
     
     temp_file.close()
     src_file.close()
@@ -195,7 +195,7 @@ def readmeSub(matchObj):
     
     scopeList = {'i' : options, 'v' : val, 'l' : gen, 't' : gen}
     
-    print('Substituting variable: ' + vr + ' - Scope: ' + pat[1])
+    logging.debug('Substituting variable: ' + vr + ' - Scope: ' + pat[1])
     
     if scope == 'i': # Input Value
         if (vr.lower() == 'createddate'):
@@ -207,7 +207,7 @@ def readmeSub(matchObj):
         return readmeArraySub(scopeList[vr[0]], vr)
     
     elif scope not in scopeList:
-        print('Scope was not recognized: ' + scope)
+        logging.warning('Scope was not recognized: ' + scope)
         return pat
     
     return str(scopeList[scope][vr.lower()])
@@ -217,7 +217,7 @@ def readmeArraySub(file, vr):
     scope = vr[0]
     space = vr.find(' ')
     key = vr[1:space]
-    # print('Key is: ' + key)
+    logging.debug('Key is: ' + key)
     struct = vr[space:]
     out = ''
     
@@ -227,8 +227,8 @@ def readmeArraySub(file, vr):
         while tmp.find('{{') > -1:
             fr = tmp.find('{{')
             la = tmp.find('}}')
-            # print('Current Source: ' + tmp + ', Fr: ' + str(fr) + ', La: ' + str(la))
-            # print('Replacing: ' + tmp[fr+2:la])
+            logging.debug('Current Source: ' + tmp + ', Fr: ' + str(fr) + ', La: ' + str(la))
+            logging.debug('Replacing: ' + tmp[fr+2:la])
             tmp = tmp[0:fr] + str(a[tmp[fr+2:la].lower()]) + tmp[la+2:]
         out = out + tmp + '\n'
     return out
@@ -245,7 +245,19 @@ if __name__ == "__main__":
     parser.add_argument('-i', '--info', dest='info', help='Very short description of the project')
     parser.add_argument('-s', '--scm', dest='scm', help='Which source control management you would like initialized', choices=['git'])
     parser.add_argument('-t', '--template', dest='template', help="Template name (also used as the name of the template's enclosing folder)", default='Generic')
+    parser.add_argument('-v', '--verbose', dest='verbosity', help='Increase verbosity (off/on/firehose)', action='count', default=0)
     args = parser.parse_args()
+    
+    ### Initialize Logging ###
+    if args.verbosity == 0:
+        l = logging.WARNING
+    elif args.verbosity == 1:
+        l = logging.INFO
+    else:
+        l = logging.DEBUG
+        
+    logging.basicConfig(level=l, format='%(asctime)s - %(levelname)s - %(message)s')
+    
     
     if ((args.name == '_stop_') or args.example):
         ### Generate Example Project/Folder ###
@@ -258,8 +270,8 @@ if __name__ == "__main__":
         
         # Set arguments with default options
         o = genDefaultOptions();
-        print('Defaults: ' + str(o))
-        print('Args: ' + str(args))
+        logging.debug('Defaults: ' + str(o))
+        logging.debug('Args: ' + str(args))
         o['name'] = args.name # This will either be true, or get a default value that won't reach this point
         o['contributors'] = o['contributors'] if getattr(args, 'contributors') is None else args.contributors
         o['directory'] = o['directory'] if getattr(args, 'directory', o['directory']) is None else args.directory
@@ -267,7 +279,7 @@ if __name__ == "__main__":
         o['info'] = o['info'] if getattr(args, 'info') is None else args.info
         o['scm'] = o['scm'] if getattr(args, 'scm', o['scm']) is None else args.scm
         o['template_name'] = o['template_name'] if getattr(args, 'template') is None else args.template
-        print('Combined: ' + str(o))
+        logging.info('Args with Defaults: ' + str(o))
         # Call Project Creation
         create_project(o)
     
