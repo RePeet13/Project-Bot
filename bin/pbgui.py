f4315c7cf1201d6d43eb4515404448a920b0ec33
Taylor Peet <repeet13@gmail.com>
2015-08-23 20:07:38 -0400

    getting more pep8 goodness in gui

--- bin/pbgui.py
+++ bin/pbgui.py
@@ -1,13 +1,21 @@
-from tkinter import *
+try:
+    # for Python2
+    import Tkinter as tk
+except ImportError:
+    # for Python3
+    import tkinter as tk
 from projectBot import genDefaultOptions as gdo
 from projectBot import create_project as cp
 from projectBot import getTemplateList as gtl
+import logging
 
 class pbgui():
 
+    logging.basicConfig(level=logging.WARNING, format='%(asctime)s - %(levelname)s - %(message)s')
+
     def __init__(self, win):
         self.main = win
-        self.frame = Frame(self.main).grid(row=0, column=0)
+        self.frame = tk.Frame(self.main).grid(row=0, column=0)
 
         self.o = gdo()
         # TODO this is a stopgap
@@ -15,28 +23,34 @@ class pbgui():
 
         templates = gtl() #get template options
 
-        self.nameVar = StringVar()
-        self.templateVar = StringVar(self.frame)
+        self.nameVar = tk.StringVar()
+        self.templateVar = tk.StringVar(self.frame)
         self.templateVar.set(templates[0]) #default value
 
-        l0 = Label(self.frame, text="Name: ").grid(row=0, column=0)
-        e0 = Entry(self.frame, textvariable=self.nameVar).grid(row=0, column=1)
-        l1 = Label(self.frame, text="Template: ").grid(row=1, column=0)
+        l0 = tk.Label(self.frame, text="Name: ").grid(row=0, column=0)
+        e0 = tk.Entry(self.frame, textvariable=self.nameVar).grid(row=0, column=1)
+        l1 = tk.Label(self.frame, text="Template: ").grid(row=1, column=0)
 
-        o0 = OptionMenu(self.frame, self.templateVar, *templates).grid(row=1, column=1)
+        o0 = tk.OptionMenu(self.frame, self.templateVar, *templates).grid(row=1, column=1)
         
-        b0 = Button(self.frame, text="Generate", command=self.generate()).grid(row=2, column = 2)
+        b0 = tk.Button(self.frame, text="Generate", command=self.generate()).grid(row=2, column = 2)
 
     def generate(self):
-        self.o['name'] = self.nameVar.get()
+        n = self.nameVar.get()
+        self.o['name'] = n
+        logging.warning('NameVar : ' + n)
         #self.nameVar.set("")
-        self.o['template_name'] = self.templateVar.get()
+
+        t = self.templateVar.get()
+        self.o['template_name'] = t
+        logging.warning('TemplateVar : ' + t)
         #self.templateVar().set("")
+
         cp(self.o)
         #self.main.destroy() #close the window
         #do more stuff               
 
-win = Tk()
+win = tk.Tk()
 win.title("Project-Bot")
 gui = pbgui(win)
 win.mainloop()
